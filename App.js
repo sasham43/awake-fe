8cd0b824fb3d7921d26b800b6021acf020f3bd77
Sasha Kramer <sashasemail@gmail.com>
2022-04-20 19:40:24 -0400

    stub out some data

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import getUserData from './data'
 
 export default function App() {
+  const [userData, setUserData] = useState([])
+
+  useEffect(() => {
+    let data = getUserData()
+    let filtered = data.filter((d, i) => i < 10)
+    setUserData(filtered)
+  }, [])
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
+      <ScrollView style={styles.data_container}>
+        {
+          userData.map(data => {
+            return (
+              <View>
+                <Text>User Data:</Text>
+                {
+                  Object.keys(data).map(key => {
+                    if(typeof data[key] === 'object') return null
+                    console.log('why',key, data[key])
+
+                    return (
+                      <View>
+                        <Text>{key}:</Text>
+                        <View>
+                          <Text>{data[key]}</Text>
+                        </View>
+                      </View>
+                    )
+                  })
+                }
+              </View>
+            )
+          })
+        }
+      </ScrollView>
     </View>
   );
 }
@@ -18,4 +54,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  data_container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flex: 1
+  }
 });
---
+++ data.js
@@ -0,0 +1,5 @@
+const data = require('./data.json')
+
+export default function getUserData(){
+    return data
+}
